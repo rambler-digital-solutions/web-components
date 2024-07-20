4916e9f09af7ffa1b5a3ed48ef8788d89f462669
andrepolischuk <me@andrepolischuk.com>
2024-07-20 21:02:05 +0000

    Deploying to gh-pages from @ rambler-digital-solutions/web-components@c52b2037bb11d1aafffad1e3e0cb03051691f4c1 🚀

---
+++ assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAAA4uuVipJrShRslIqSk3PLC5JLQrKzy9R0lEqSCzJULJSSivNSy7JzM8r1keW18soyc1R0lHKzsxLUbIyM6nVwTAmuKQyJ7UYn0EQFXiMKs0j5CZUFRhGxQIAhJdcwd4AAAA="
